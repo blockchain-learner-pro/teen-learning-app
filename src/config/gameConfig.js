d27cc725ed67d17ca9796f4ee0c8f9538e2a3def
echowhisper <aroha.heitia@yahoo.com>
2026-07-18 19:42:53 +1200

    fix: add missing gameConfig for gameMath import

---
+++ src/config/gameConfig.js
@@ -0,0 +1,15 @@
+export const GAME_CONFIG = {
+  levels: [
+    { name: 'Apprentice', bossHp: 1000, questions: 10 },
+    { name: 'Warrior', bossHp: 2000, questions: 15 },
+    { name: 'Champion', bossHp: 3500, questions: 20 },
+  ],
+  maxEnergy: 120,
+  startingGold: 320,
+  startingGems: 15,
+  xpPerLevel: 50,
+  comboMultiplier: 10,
+  baseDamage: 12,
+  damagePerDifficulty: 4,
+  maxComboBonus: 4,
+};
